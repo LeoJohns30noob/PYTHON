810cc612d0a23ee968463bd95083c74268f1046b
LeoJohnsGitHub <214759374+LeoJohns30noob@users.noreply.github.com>
2026-05-27 21:44:03 +0000

    Agregar script principal

---
+++ script_principal.py
@@ -0,0 +1,21 @@
+print ("¡Hola, Mundo!")
+#Estructuras de control
+
+edad = 60 
+
+if edad < 18:
+    print ("Eres menor de edad.")
+
+elif edad >=18 and edad <59:
+    print("Eres un adulto.")
+
+elif edad > 15 and edad <15:
+    print("Mayor de 15 años.")
+
+elif edad == 60:
+    print("¡Feliz 60 cumpleaños!.")
+
+else :
+    print("Eres adulto mayor.")
+                 
+        
